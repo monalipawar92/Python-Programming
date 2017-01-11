7f4454baf2636d0e76d74d11dd4a83883f595d1b
monalipawar92 <monaligpawar@gmail.com>
2017-01-11 22:17:19 +0530

    comparison.py and prime.py file is added

---
+++ comparison.py
@@ -0,0 +1,41 @@
+a = 21
+b = 10
+c = 0
+
+if(a == b):
+  print("line1 - a is equal to b")
+
+else:
+  print("line1 - a is not equal to b")  
+
+
+if(a != b):
+  print("line1 - a is not equal to b")
+
+else:
+  print("line1 - a is  equal to b")  
+
+
+if(a < b):
+  print("line1 - a is less than b")
+
+else:
+  print("line1 - a is greater than  b")  
+
+if(a > b):
+  print("line1 - a is greater than  b")
+
+else:
+  print("line1 - a is less than  b")  
+
+if(a < b):
+  print("line1 - a is less than b")
+
+else:
+  print("line1 - a is greater than  b")  
+
+if(a >= b):
+  print("line1 - a is greater than  b")
+
+else:
+  print("line1 - a is less than  b")  
