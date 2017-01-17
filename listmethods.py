07eb065025313e65b8dd60fc846f8cf1cd6d72f2
monalipawar92 <monaligpawar@gmail.com>
2017-01-17 19:34:32 +0530

    listmethods.py,listmethods1.py and listupdate file is added.

---
+++ listmethods.py
@@ -0,0 +1,15 @@
+list1= ['computr', 1991, 60]
+list2 = ['Mechanical',1987,120]
+list3 = [10,12,14,15]
+
+print (list1)
+print (list2)
+
+a = len (list1)                   #to find length of list
+print ('length of list is:',a)
+
+m = max(list3)                     #find maximum number
+print ('maximum number is: ',m)
+m = min(list3)                     #find minimum number
+print ('minimum number is: ',m)
+
---
+++ listupdate.py
@@ -0,0 +1,12 @@
+
+list = ['computr', 1991, 60]
+
+print (list)
+
+list[2]= 120       #update liat
+
+print (list)
+
+del list[2]      #delete item from list
+
+print (list)
