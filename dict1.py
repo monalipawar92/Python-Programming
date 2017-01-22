916ec06fd9adb7039fc1cab3631149608dc0de7b
monalipawar92 <monaligpawar@gmail.com>
2017-01-22 22:44:11 +0530

    dict1.py,dicttmethods.py and dictupdate file is added.

---
+++ dict1.py
@@ -0,0 +1,10 @@
+
+dict = {'name': 'sushant','id':1, 'branch':'computer'}
+print (dict)
+print (dict.keys())
+print (dict.values())
+
+dict1 = {'name': 'Jitendra','id':2, 'branch':'computer'}
+print (dict1)
+print (dict1.keys())
+print (dict1.values())
---
+++ dictupdate.py
@@ -0,0 +1,22 @@
+
+dict = {'Name': 'sushant', 'Age':26,'Branch':'Computer'}
+
+print (dict)
+
+dict['Age'] = 27   #update dictionary
+
+dict ['college'] = 'SSBT COET'   #Add new entry
+
+print (dict)
+
+del dict['Branch']  #delete entry
+
+print (dict)
+
+dict.clear();        #remove all entries
+
+print (dict)
+
+del dict           #delete entire dictionary
+
+print (dict)
